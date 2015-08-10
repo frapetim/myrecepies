32cb2c65668d68eed53ec5aa4d0195c596227ec5
fra.pet <frapetim@gmail.com>
2015-08-10 00:58:53 +0000

    added recipe tests

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -10,5 +10,55 @@ class RecipeTest < ActiveSupport::TestCase
       assert @recipe.valid?
     end
     
+    test 'title should be present' do
+      @recipe.title = ""
+      assert_not @recipe.valid?
+    end
     
+    test 'title should be not too long' do 
+      @recipe.title = "a" * 101
+      
+      assert_not @recipe.valid?
+    end
+    
+    test 'title should not be too short' do
+      @recipe.title = 'aaaa'
+      assert_not @recipe.valid?
+    end
+    
+    test 'summary should be present' do
+      @recipe.summary = ""
+      
+      assert_not @recipe.valid?
+    end
+    
+    test 'summary should not be too long' do 
+      @recipe.summary = "a" * 151
+      
+      assert_not @recipe.valid?
+    end
+    
+    test 'summary should not be too short' do
+      @recipe.summary = "a" * 9
+      
+      assert_not @recipe.valid?
+    end
+    
+    test 'description should be present' do
+      @recipe.description = ""
+      
+      assert_not @recipe.valid?
+    end
+    
+     test 'description should not be too long' do 
+      @recipe.description = "a" * 501
+      
+      assert_not @recipe.valid?
+    end
+    
+    test 'description should not be too short' do
+      @recipe.description = "a" * 19
+      
+      assert_not @recipe.valid?
+    end
 end
